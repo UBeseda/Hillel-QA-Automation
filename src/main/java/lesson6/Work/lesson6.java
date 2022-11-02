da8f163d905079fe4828d00b461e3c37925c3cc4
Julia <ubeseda@gmail.com>
2022-11-03 01:06:29 +0200

    -add files: Animal, Сat (objects) and lesson6 (running code)

--- src/main/java/lesson6/Work/lesson6.java
+++ src/main/java/lesson6/Work/lesson6.java
@@ -4,7 +4,7 @@ import lesson6.Objects.Cat;
 
 public class lesson6 { //main
     public static void main(String[] args) { //static
-        //Вивести 1 констуртор зі всіма параметрами
+        //Вивести 1 конструктор зі всіма параметрами
         Cat cat = new Cat(false, "mouse", 4, "black");
         System.out.println("Vegetarian: " + cat.isVegetarian() + "\n" + "Eats: " + cat.getEats() + "\n"
                 + "noOfLegs: " + cat.getNoOfLegs() + "\n" + "color: " + cat.getColor());
@@ -12,7 +12,7 @@ public class lesson6 { //main
         // для красоти перенесення - щоб в консолі все не зліпалось
         System.out.println("\n");
 
-        //Вивести 2 констуртор зі всіма параметрами, але колір - за замовченням
+        //Вивести 2 конструктор зі всіма параметрами, але колір - за замовченням
         Cat cat1 = new Cat(true, "gress", 4 );
         System.out.println("Vegetarian: " + cat1.isVegetarian() + "\n" + "Eats: " + cat1.getEats() + "\n"
                 + "noOfLegs: " + cat1.getNoOfLegs() + "\n" + "color: " + cat1.getColor());
